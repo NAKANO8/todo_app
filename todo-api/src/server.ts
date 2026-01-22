d6e9e6691b7f3e81732dc0b68739180502f6b93e
NAKANO8 <hika110308@gmail.com>
2026-01-22 19:58:05 +0900

    add-container-db-api-web

--- todo-api/src/server.ts
+++ todo-api/src/server.ts
@@ -2,7 +2,9 @@
 import "dotenv/config";
 import { app } from "./app";
 
-app.listen({ port: 3000 }, (err, address) => {
+const PORT = Number(process.env.PORT) || 3001;
+
+app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
